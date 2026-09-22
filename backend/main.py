88d3d1a970e942a113021702e1f82ae6e29dcba2
hiroto <khiroto324@gmail.com>
2026-09-23 02:41:14 +0900

    modify a bit so I can use vercel app

--- backend/main.py
+++ backend/main.py
@@ -8,11 +8,16 @@ from core.config import settings
 app = FastAPI(title="GitHub Email Scraper", version="1.0.0")
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=settings.origins,
+    allow_origins=[
+        "https://github-email-scraper-hiroto.vercel.app",
+        "http://localhost:3000",
+        "http://localhost:5173",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
 app.include_router(router)
 app.include_router(ws_router)
 
